43e336094eef6c46ec24bdb9006d08e84018268c
LouPingg <lechatdesboisbois@hotmail.com>
2025-11-11 19:55:57 +0100

    depart

--- models/Whitelist.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const whitelistSchema = new mongoose.Schema({
-  nickname: { type: String, required: true, unique: true },
-});
-
-export default mongoose.model("Whitelist", whitelistSchema);
